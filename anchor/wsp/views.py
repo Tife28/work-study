f0a98651eedfe252fa73e18094bdd20b968b29c6
Tife28 <boludayo@ekunstech.com>
2022-08-04 14:46:59 +0100

    Modified

--- anchor/wsp/views.py
+++ anchor/wsp/views.py
@@ -22,18 +22,20 @@ def show(request):
     return render(request,"show.html",{'students':students})  
 def edit(request, id):  
     student = Student.objects.get(id=id)  
-    if request.method == "POST":  
-        form = RecommendationForm(request.POST)
-        print(dict(form.errors))
-        if form.is_valid():  
-            try:  
-                form.save() 
-                return HttpResponse("<h3>Application successfully submitted!</h3><br><center><a href='/show' class='btn btn-primary'>Update another Work Study Profile</a></center>")
-            except:  
-                pass  
-    else:  
-        form = RecommendationForm()   
-    return render(request,'edit.html', {'student':student, 'form': form })  
+    # if request.method == "POST":  
+    #     print(dict(form.errors))
+    #     form = RecommendationForm(request.POST)
+    #     print(dict(form.errors))
+    #     if form.is_valid():  
+    #         try:  
+    #             form.save() 
+    #             return HttpResponse("<h3>Application successfully submitted!</h3><br><center><a href='/show' class='btn btn-primary'>Update another Work Study Profile</a></center>")
+    #         except:  
+    #             pass  
+    # else:  
+    #     form = RecommendationForm()   
+    #     print(dict(form.errors))
+    return render(request,'edit.html', {'student':student})#, 'form': form })  
 def update(request, id):  
     student = Student.objects.get(id=id)  
     recommendation=None
@@ -41,19 +43,21 @@ def update(request, id):
         recommendation = student.recommendation
     except:
         pass
-    form2 = RecommendationForm(request.POST, instance=recommendation)
+    form = StudentForm(instance=student)
+    form2 = RecommendationForm(instance= recommendation)
     
     if request.method == 'POST':
         form = StudentForm(request.POST, instance = student) 
-        form2 = RecommendationForm(request.POST, instance=recommendation)
+        form2 = RecommendationForm(request.POST, request.FILES, instance=recommendation)
+        print(dict(form2.errors),dict(form.errors), request.POST, request.FILES)
         if form.is_valid() and form2.is_valid():  
-            recommendation_data = form.save(commit=False)
+            recommendation_data = form2.save(commit=False)
             recommendation_data.student = student
             recommendation_data.save()  
 
             form.save()
             return redirect("/show")  
-    return render(request, 'edit.html', {'student': student}) 
+    return render(request, 'edit.html', {'student': student, 'form': form2, 'studentform': form}) 
 def destroy(request, id):  
     student = Student.objects.get(id=id)  
     student.delete()  
